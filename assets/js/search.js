f9b5097b454715d90aed64c3a9960aba44f97326
dokongg <66020034+dokongg@users.noreply.github.com>
2021-04-30 17:16:40 +0900

    Update search.js

--- assets/js/search.js
+++ assets/js/search.js
@@ -42,7 +42,7 @@ var displaySearchResults = function(results, store, initalize) {
     var currentPageGroup = Number($('#currentPageGroup').val());
 
     if (results.length) { // Are there any results?
-        var appendString = '<div class="search-results">총 <span> class="search-count">' + $('#totalCount').val() + '</span>개의 결과가 있습니다.</div>';
+        var appendString = '<div class="search-results">총 <span class="search-count">' + $('#totalCount').val() + '</span>개의 결과가 있습니다.</div>';
 
         for (var i = startIndex; i < results.length; i++) {  // Iterate over the results
             var item = store[results[i].ref];
